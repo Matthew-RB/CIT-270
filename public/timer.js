97c6d9e4981064c50573a710d9389e68f297711b
Matthew Brunson <matt0808@byui.edu>
2023-02-03 13:47:36 -0700

    made changes to where we add cookies

--- public/timer.js
+++ public/timer.js
@@ -19,10 +19,16 @@
         counterbutton = document.getElementById('counterbutton');
         let hash= location.hash;//will include the #
         let hashparts = hash.split("#");
-        if (hashparts.length < 2) {
+        const cookies = document.cookie;
+        const cookieValue = document.cookie
+            .split('; ')
+            .find((row) => row.startsWith('stedicookie='))
+            ?.split('=')[1];
+        console.log("cookieValue", JSON.stringify(cookieValue));
+        if (cookieValue == null) {
             window.location="/"; //there is no login token on the url, so they must not have logged in yet, we will help redirect them here
         } else {
-            usertoken = hashparts[1];// the url should look like https://stedi.me/timer.html#4c2286a7-8fdc-47c5-b972-739769554c88
+            usertoken = cookieValue; 
             validateToken();//check if token is expired, if not display the email, if expired send to login
         }
     });
@@ -44,7 +50,7 @@
                 401: () => window.location.href="/",
             },
             headers: { "suresteps.session.token": usertoken},
-            contentType: "application/text",
+            contentType: "application/json",
             dataType: 'text'
         });
 
@@ -78,7 +84,7 @@
         let tokenEmail="";
         $.ajax({
            type: 'GET',
-            url: '/validate/'+usertoken,
+            url: '/validate',
             success: function(data){
                if (data==""){
                  window.location="/"
@@ -107,7 +113,7 @@
         	startandstop();
         	let testTime = stepTime-starttime;
             let rapidStepTest = {
-               token: usertoken,
+               //token: usertoken,
                startTime: starttime,
                stopTime: stepTime,
                testTime: testTime,
--- server.js
+++ server.js
@@ -12,12 +12,32 @@ const Redis = require('redis'); // the libary
 
 const redisClient = Redis.createClient({url:"redis://127.0.0.1:6379"}); //this points to redis
 
+const cookieParser = require("cookie-parser");
+
+
 app.use(bodyParser.json()); //This looks for incoming data
 
 app.use(express.static('public'));
 
+app.use(cookieParser());
+
+app.post('/rapidsteptest', async (req, res)=>{
+    const steps = req.body;
+    await redisClient.zAdd("Steps", steps, 0);
+    console.log('Steps', steps);
+    res.send('saved');
+});
+
+
 app.get("/", (req, res) => {
     res.send("Hello Matthew");
+});
+
+app.get("/validate", async(req, res) =>{
+    const loginToken = req.cookies.stedicookie;
+    console.log("loginToken", loginToken);
+    const loginUser = await redisClient.hGet('TokenMap', loginToken);
+    res.send(loginUser);
 });
 
 app.post('/login',async (req, res) =>{
@@ -27,6 +47,8 @@ app.post('/login',async (req, res) =>{
     const correctPassword = await redisClient.hGet('UserMap',loginUser); //gets correct password from redis
     if (correctPassword == loginPassword){
         const loginToken = uuidv4();
+        await redisClient.hSet('TokenMap', loginToken, loginUser);
+        res.cookie('stedicookie', loginToken);
         res.send(loginToken);
     } else {
         res.status(401);
